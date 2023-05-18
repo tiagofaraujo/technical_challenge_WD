e3df5230242c34410407ed23c9e4b77c574c496e
tiagofaraujo <johndoe@example.com>
2023-05-18 16:55:32 +0100

    changing phone list and details to same page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AllPhones from './pages/AllPhones';
-import DetailsPhone from './pages/DetailsPhone';
-/* import Error from './pages/Error';
-import NotFound from './pages/NotFound'; */
-
 
 
 function App() {
@@ -12,9 +8,6 @@ return (
   <div className="App">
       <Routes>
       <Route path="/" element={<AllPhones />} />
-      <Route path="/phones/:phoneId" element={<DetailsPhone />} />
-    {/* <Route path="/error" element={<Error/>} />
-    <Route path="*" element={<NotFound/>} /> */}
     </Routes>
 </div>
 );
--- Client/src/pages/AllPhones.jsx
+++ Client/src/pages/AllPhones.jsx
@@ -2,45 +2,74 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Row, Col, Button} from 'react-bootstrap';
+import { Card, Row, Col, Button } from 'react-bootstrap';
 
 const API_URL = 'http://localhost:5005';
 
 export default function AllPhones() {
-    const [phones, setPhones] = useState([]);
-    
-    useEffect(() => {
-        axios
-        .get(`${API_URL}/api/phones`)
-        .then((response) => {
-            setPhones(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }, []);
- 
-    return (
-        <div>
-          <h1 className="text-center mb-4">The Phone Cave Catalog App</h1>
-          <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
-            {phones.map((phone) => (
-              <Col key={phone.id}>
-                <Card>
-                  <Card.Img variant="top" src={`/images/${phone.imageFileName}`} alt={phone.name} />
-                  <Card.Body>
-                    <Card.Title>{phone.name}</Card.Title>
-                    <Card.Text>
-                      <p>{phone.manufacturer}</p>
-                    </Card.Text>
-                    <Link to={`/phones/${phone.id}`}>
-                      <Button variant="primary">More Info</Button>
-                    </Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
-      );
-    }
+  const [phones, setPhones] = useState([]);
+  const [selectedPhone, setSelectedPhone] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/phones`)
+      .then((response) => {
+        setPhones(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleMoreInfoClick = (phone) => {
+    setSelectedPhone(phone);
+  };
+
+  const handleBackClick = () => {
+    setSelectedPhone(null);
+  };
+
+  return (
+    <div>
+      <h1 className="text-center mb-4">The Phone Cave Catalog App</h1>
+      <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
+        {phones.map((phone) => (
+          <Col key={phone.id}>
+            <Card>
+              <Card.Img variant="top" src={`/images/${phone.imageFileName}`} alt={phone.name} />
+              <Card.Body>
+                <Card.Title>{phone.name}</Card.Title>
+                <Card.Text>
+                  <p>{phone.manufacturer}</p>
+                </Card.Text>
+                {selectedPhone === phone ? (
+                  <div>
+                    <p>Description: {phone.description}</p>
+                    <p>Color: {phone.color}</p>
+                    <p>Price: {phone.price}</p>
+                    <p>Screen: {phone.screen}</p>
+                    <p>Processor: {phone.processor}</p>
+                    <p>Ram: {phone.ram}</p>
+                  </div>
+                ) : (
+                  <Button variant="primary" onClick={() => handleMoreInfoClick(phone)}>
+                    More Info
+                  </Button>
+                )}
+              </Card.Body>
+              {selectedPhone === phone && (
+                <Card.Text>
+                  <Button variant="primary" onClick={handleBackClick}>
+                    Back
+                  </Button>
+                
+                </Card.Text>
+              )}
+              <br />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+}
