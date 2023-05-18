54f5d5d08e76c091b6b1fdd94681d7d26a860eb3
tiagofaraujo <johndoe@example.com>
2023-05-18 14:46:32 +0100

    phone details solved

--- Client/src/pages/AllPhones.jsx
+++ Client/src/pages/AllPhones.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Row, Col, Button} from 'react-bootstrap';
 
 const API_URL = 'http://localhost:5005';
 
@@ -18,27 +19,28 @@ export default function AllPhones() {
             console.log(error);
         });
     }, []);
-    
+ 
     return (
         <div>
-        <h1>All Phones</h1>
-        {phones.map((phone) => {
-            return (
-            <div key={phone.id}>
-                <Link to={`/phones/${phone.id}`}>
-                <h3>{phone.name}</h3>
-                </Link>
-                <img src={`/images/${phone.imageFileName}`} alt={phone.name} />
-                <p>Manufacturer: {phone.manufacturer}</p>
-                <p>Description: {phone.description}</p>
-                <p>Color: {phone.color}</p>
-                <p>Price: {phone.price}</p>
-                <p>Screen: {phone.screen}</p>
-                <p>Processor: {phone.processor}</p>
-                <p>Ram: {phone.ram}</p>
-            </div>
-            );
-        })}
+          <h1 className="text-center mb-4">The Phone Cave Catalog App</h1>
+          <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
+            {phones.map((phone) => (
+              <Col key={phone.id}>
+                <Card>
+                  <Card.Img variant="top" src={`/images/${phone.imageFileName}`} alt={phone.name} />
+                  <Card.Body>
+                    <Card.Title>{phone.name}</Card.Title>
+                    <Card.Text>
+                      <p>{phone.manufacturer}</p>
+                    </Card.Text>
+                    <Link to={`/phones/${phone.id}`}>
+                      <Button variant="primary">More Info</Button>
+                    </Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </div>
-    );
+      );
     }
--- Client/src/pages/DetailsPhone.jsx
+++ Client/src/pages/DetailsPhone.jsx
@@ -1,42 +1,51 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import AllPhones from './AllPhones';
+import { Link, useParams } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Card } from 'react-bootstrap';
 
 const API_URL = 'http://localhost:5005';
 
 export default function DetailsPhone() {
-    const [phone, setPhone] = useState([]);
-    const { phoneId } = useParams();
-    
-    useEffect(() => {
-        axios
-        .get(`${API_URL}/api/phones/${phoneId}`)
-        .then((response) => {
-            setPhone(response.data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    }, [phoneId]);
-    
-    return (
-        <div>
-        <h1>Details Phone</h1>
-        <div key={phone._id}>
-            <Link to={`/phones/${phone._id}`}>
-            <h3>{phone.name}</h3>
-            </Link>
-            <p>Manufacturer: {phone.manufacturer}</p>
-            <p>Description: {phone.description}</p>
-            <p>Color: {phone.color}</p>
-            <p>Price: {phone.price}</p>
-            <p>Screen: {phone.screen}</p>
-            <p>Processor: {phone.processor}</p>
-            <p>Ram: {phone.ram}</p>
+  const [phone, setPhone] = useState({});
+  const { phoneId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/phones/${phoneId}`)
+      .then((response) => {
+        setPhone(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [phoneId]);
+
+  return (
+    <div className="container">
+      <h1 className="mt-4">Details Phone</h1>
+      <div className="card mt-4">
+        <div className="card-body">
+          <Card.Img
+            variant="top"
+            src={`/images/${phone.imageFileName}`}
+            alt={phone.name}
+            style={{ width: '200px', height: 'auto' }} // Specify the desired dimensions
+          />
+          <h3 className="card-title">{phone.name}</h3>
+          <p className="card-text">Manufacturer: {phone.manufacturer}</p>
+          <p className="card-text">Description: {phone.description}</p>
+          <p className="card-text">Color: {phone.color}</p>
+          <p className="card-text">Price: {phone.price}</p>
+          <p className="card-text">Screen: {phone.screen}</p>
+          <p className="card-text">Processor: {phone.processor}</p>
+          <p className="card-text">Ram: {phone.ram}</p>
         </div>
-        </div>
-    );
-    }
-    
+      </div>
+      <br />
+      <Link to="/" className="btn btn-primary mb-3">
+        Back to Phone List
+      </Link>
+    </div>
+  );
+}
